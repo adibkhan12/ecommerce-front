a369dc17061aba9ef882fd518bc0990db29775fe
Adib Khan <adeebk418@gmail.com>
2025-02-01 00:14:53 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler:{
-    styledComponents:true,
-  }
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "adib-next-ecommerce.s3.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
